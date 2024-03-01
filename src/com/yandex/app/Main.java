a2d081584142d7775c9e726d7d890c40ee06d182
Max <maxusjob@ya.ru>
2024-03-01 14:10:06 +0100

    main cleanup

--- src/com/yandex/app/Main.java
+++ src/com/yandex/app/Main.java
@@ -1,6 +1,5 @@
 package com.yandex.app;
 
-import com.yandex.app.enums.Status;
 import com.yandex.app.model.Epic;
 import com.yandex.app.model.Subtask;
 import com.yandex.app.model.Task;
@@ -10,65 +9,16 @@ import com.yandex.app.service.TaskManager;
 public class Main {
 
     public static void main(String[] args) {
-        Task task1 = new Task("task1", "task description1");
-        Task task2 = new Task("task2", "task description2");
-        Task updateTask = new Task("task updated", "task description updated");
-
-        Epic epic1 = new Epic("epic1", "epic description1");
-        Epic epic2 = new Epic("epic2", "epic description2");
-
-
         TaskManager taskManager = Managers.getDefault();
-
+        Task task1 = new Task("task1", "task description1");
+        Epic epic1 = new Epic("epic1", "epic description1");
         taskManager.addTask(task1);
-        Task addedTask = taskManager.addTask(task2);
-        updateTask.setId(addedTask.getId());
-        taskManager.updateTask(updateTask);
-        //System.out.println(taskManager.getAllTasks());
-
-
         taskManager.addEpic(epic1);
-        taskManager.addEpic(epic2);
-
-        Epic updateEpic = new Epic("epic update", "epic description update");
-        updateEpic.setId(epic2.getId());
-        taskManager.updateEpic(updateEpic);
-
-        Subtask subtask1 = new Subtask("subtask1","subtask description1", epic1.getId());
-        Subtask subtask2 = new Subtask("subtask2","subtask description2", epic1.getId());
-        Subtask subtask3 = new Subtask("subtask3","subtask description3", epic2.getId());
-
+        Subtask subtask1 = new Subtask("subtask1", "subtask description1", epic1.getId());
         taskManager.addSubtask(subtask1);
-        taskManager.addSubtask(subtask2);
-        taskManager.addSubtask(subtask3);
-        //System.out.println(taskManager.getAllSubtasks());
-        //System.out.println(taskManager.getAllEpics());
-
-        Subtask updateSubtask1 = new Subtask("subtask update1","subtask description update", subtask1.getEpicId(), Status.NEW);
-        updateSubtask1.setId(subtask1.getId());
-
-        Subtask updateSubtask2 = new Subtask("subtask update2","subtask description update", subtask2.getEpicId(), Status.DONE);
-        updateSubtask2.setId(subtask2.getId());
-        taskManager.updateSubtask(updateSubtask1);
-        taskManager.updateSubtask(updateSubtask2);
-
-        //System.out.println(taskManager.getAllEpics());
-        //System.out.println(taskManager.getEpicSubtasks(epic1.getId()));
-
-        //taskManager.deleteEpic(epic1.getId());
-        //taskManager.deleteSubtask(subtask2.getId());
-        System.out.println("---------------------------------");
-        //System.out.println(taskManager.getAllSubtasks());
-
-        //taskManager.deleteSubtask(subtask2.getId());
-        //taskManager.deleteSubtask(subtask1.getId());
-        //System.out.println(taskManager.getAllEpics());
-
         taskManager.getTaskById(task1.getId());
-        taskManager.getTaskById(task2.getId());
         taskManager.getSubtaskById(subtask1.getId());
         taskManager.getEpicById(epic1.getId());
-        taskManager.getTaskById(task2.getId());
         System.out.println("History:");
         for (Task task : taskManager.getHistory()) {
             System.out.println(task);
