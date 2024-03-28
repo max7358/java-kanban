d7ea1eae6d58a67e4117ddf725bf8860558ec801
max7358 <157724555+max7358@users.noreply.github.com>
2024-03-28 14:20:37 +0100

    Sprint 6 solution (#1)
    * added blank method remove(int id)
    
    * switched history to linked list impl
    added remove method
    updated tests
    
    * switched history to linked list impl
    added remove method
    updated tests
    
    * history test
    
    * github api-tests
    
    * code style check fix
    
    * code style check fix
    
    * code style check fix
    
    * updated tests
    
    * GitHub check fix: moved tests to test folder
    
    * review fixes:
    - node class simplify
    - better test names
    
    * test addToHistory logic update

--- src/com/yandex/app/service/InMemoryTaskManager.java
+++ src/com/yandex/app/service/InMemoryTaskManager.java
@@ -5,14 +5,12 @@ import com.yandex.app.model.Epic;
 import com.yandex.app.model.Subtask;
 import com.yandex.app.model.Task;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 public class InMemoryTaskManager implements TaskManager {
-    final private HashMap<Integer, Task> tasks;
-    final private HashMap<Integer, Subtask> subtasks;
-    final private HashMap<Integer, Epic> epics;
+    private final HashMap<Integer, Task> tasks;
+    private final HashMap<Integer, Subtask> subtasks;
+    private final HashMap<Integer, Epic> epics;
     HistoryManager historyManager;
     IdGenerator idGenerator = new IdGenerator();
 
@@ -33,6 +31,7 @@ public class InMemoryTaskManager implements TaskManager {
     //delete all tasks
     @Override
     public void deleteAllTasks() {
+        removeTasksFromHistory(tasks.keySet());
         tasks.clear();
     }
 
@@ -63,6 +62,7 @@ public class InMemoryTaskManager implements TaskManager {
     //delete task by id
     @Override
     public Task deleteTaskById(int id) {
+        historyManager.remove(id);
         return tasks.remove(id);
     }
 
@@ -97,9 +97,10 @@ public class InMemoryTaskManager implements TaskManager {
             Epic epic = epics.get(s.getEpicId());
             if (epic != null) {
                 epic.removeSubtaskId(s.getId());
-                updateEpicStatus(epics.get(s.getEpicId()));
+                updateEpicStatus(epic);
             }
         });
+        removeTasksFromHistory(subtasks.keySet());
         subtasks.clear();
     }
 
@@ -109,6 +110,7 @@ public class InMemoryTaskManager implements TaskManager {
         int epicId = subtasks.get(id).getEpicId();
         epics.get(epicId).removeSubtaskId(id);
         updateEpicStatus(epics.get(epicId));
+        historyManager.remove(id);
         subtasks.remove(id);
     }
 
@@ -192,7 +194,9 @@ public class InMemoryTaskManager implements TaskManager {
     //delete all epics and all linked subtasks
     @Override
     public void deleteAllEpics() {
+        removeTasksFromHistory(epics.keySet());
         epics.clear();
+        removeTasksFromHistory(subtasks.keySet());
         subtasks.clear();
     }
 
@@ -200,13 +204,17 @@ public class InMemoryTaskManager implements TaskManager {
     @Override
     public void deleteEpicById(int id) {
         deleteSubtasksByIds(epics.get(id).getSubtaskIds());
+        historyManager.remove(id);
         epics.remove(id);
     }
 
     //delete subtasks by id
     //private method only for "delete com.yandex.app.model.Epic" cases
     private void deleteSubtasksByIds(ArrayList<Integer> subtaskIds) {
-        subtaskIds.forEach(subtasks::remove);
+        subtaskIds.forEach(id -> {
+            historyManager.remove(id);
+            subtasks.remove(id);
+        });
     }
 
     //update epic
@@ -218,5 +226,10 @@ public class InMemoryTaskManager implements TaskManager {
 
     public List<Task> getHistory() {
         return historyManager.getHistory();
+    }
+
+    //remove tasks from history by id
+    private void removeTasksFromHistory(Set<Integer> taskIds) {
+        taskIds.forEach(historyManager::remove);
     }
 }
---
+++ test/service/InMemoryHistoryManagerTest.java
@@ -0,0 +1,179 @@
+package service;
+
+import com.yandex.app.model.Epic;
+import com.yandex.app.model.Subtask;
+import com.yandex.app.model.Task;
+import com.yandex.app.service.HistoryManager;
+import com.yandex.app.service.TaskManager;
+import com.yandex.app.utility.Managers;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+class InMemoryHistoryManagerTest {
+
+    HistoryManager historyManager;
+
+    @BeforeEach
+    void setUp() {
+        historyManager = Managers.getDefaultHistory();
+    }
+
+    @Test
+    void addToHistoryNoDuplicates() {
+        Task task1 = new Task("task1", "task description1");
+        task1.setId(1);
+        Epic epic1 = new Epic("epic1", "epic description1");
+        epic1.setId(2);
+        Subtask subtask1 = new Subtask("subtask1", "task description1", epic1.getId());
+        subtask1.setId(3);
+        historyManager.add(task1);
+        historyManager.add(epic1);
+        historyManager.add(subtask1);
+        historyManager.add(task1);
+        List<Task> history = historyManager.getHistory();
+        Assertions.assertEquals(3, history.size());
+        Assertions.assertTrue(history.contains(task1));
+        Assertions.assertTrue(history.contains(epic1));
+        Assertions.assertTrue(history.contains(subtask1));
+    }
+
+    @Test
+    void removeFromHistory() {
+        Task task1 = new Task("task1", "task description1");
+        task1.setId(1);
+        Task task2 = new Task("task2", "task description2");
+        task1.setId(2);
+        Task task3 = new Task("task3", "task description3");
+        task3.setId(3);
+        historyManager.add(task1);
+        historyManager.add(task2);
+        historyManager.add(task3);
+        historyManager.remove(task2.getId());
+        List<Task> history = historyManager.getHistory();
+        Assertions.assertEquals(2, history.size());
+        Assertions.assertTrue(history.contains(task1));
+        Assertions.assertTrue(history.contains(task3));
+    }
+
+    @Test
+    void historyObjectUnchanged() {
+        TaskManager taskManager = Managers.getDefault();
+        Task task1 = new Task("task1", "task description1");
+        taskManager.addTask(task1);
+        taskManager.getTaskById(task1.getId());
+        List<Task> historyOld = taskManager.getHistory();
+
+        Task taskUpdate = new Task("taskUp", "task descriptionUp");
+        taskUpdate.setId(task1.getId());
+        taskManager.updateTask(taskUpdate);
+        taskManager.getTaskById(task1.getId());
+        Assertions.assertEquals(task1, historyOld.getFirst());
+    }
+
+    @Test
+    void historyUpdatesWithTaskUpdate() {
+        TaskManager taskManager = Managers.getDefault();
+        Task task1 = new Task("task1", "task description1");
+        taskManager.addTask(task1);
+        taskManager.getTaskById(task1.getId());
+        Assertions.assertEquals(1, taskManager.getHistory().size());
+        Assertions.assertEquals(taskManager.getHistory().getFirst(), task1);
+        Task taskUpdate = new Task("taskUp", "task descriptionUp");
+        taskUpdate.setId(task1.getId());
+        taskManager.updateTask(taskUpdate);
+        taskManager.getTaskById(task1.getId());
+        Assertions.assertEquals(1, taskManager.getHistory().size());
+        Assertions.assertEquals(taskManager.getHistory().getFirst(), taskUpdate);
+    }
+
+    @Test
+    void historyCorrectOrderForSameTasks() {
+        TaskManager taskManager = Managers.getDefault();
+        Task task1 = new Task("task1", "task description1");
+        Task task2 = new Task("task2", "task description2");
+        Task task3 = new Task("task3", "task description3");
+        Epic epic1 = new Epic("epic1", "epic description1");
+        taskManager.addEpic(epic1);
+        Subtask subtask1 = new Subtask("subtask1", "subtask description1", epic1.getId());
+        taskManager.addSubtask(subtask1);
+        taskManager.addTask(task1);
+        taskManager.addTask(task2);
+        taskManager.addTask(task3);
+        taskManager.getTaskById(task2.getId());
+        taskManager.getTaskById(task2.getId());
+        taskManager.getTaskById(task1.getId());
+        taskManager.getTaskById(task1.getId());
+        taskManager.getTaskById(task2.getId());
+        taskManager.getSubtaskById(subtask1.getId());
+        taskManager.getTaskById(task3.getId());
+        taskManager.getEpicById(epic1.getId());
+        taskManager.getTaskById(task2.getId());
+        taskManager.getSubtaskById(subtask1.getId());
+        taskManager.getTaskById(task1.getId());
+        taskManager.getTaskById(task3.getId());
+        taskManager.getTaskById(task1.getId());
+        List<Task> history = taskManager.getHistory();
+        List<Task> testHistory = List.of(epic1, task2, subtask1, task3, task1);
+        Assertions.assertEquals(testHistory, history);
+    }
+
+    @Test
+    void historyOrderDeleteTask() {
+        TaskManager taskManager = Managers.getDefault();
+        Task task1 = new Task("task1", "task description1");
+        Task task2 = new Task("task2", "task description2");
+        Task task3 = new Task("task3", "task description3");
+        Task task4 = new Task("task4", "task description4");
+        Epic epic1 = new Epic("epic1", "epic description1");
+        taskManager.addEpic(epic1);
+        Subtask subtask1 = new Subtask("subtask1", "subtask description1", epic1.getId());
+        taskManager.addSubtask(subtask1);
+        taskManager.addTask(task1);
+        taskManager.addTask(task2);
+        taskManager.addTask(task3);
+        taskManager.addTask(task4);
+        taskManager.getTaskById(task1.getId());
+        taskManager.getTaskById(task1.getId());
+        taskManager.getTaskById(task4.getId());
+        taskManager.getTaskById(task3.getId());
+        taskManager.getTaskById(task2.getId());
+        taskManager.getTaskById(task2.getId());
+        taskManager.getSubtaskById(subtask1.getId());
+        taskManager.getEpicById(epic1.getId());
+        taskManager.getSubtaskById(subtask1.getId());
+        taskManager.deleteTaskById(task2.getId());
+        taskManager.getTaskById(task3.getId());
+        taskManager.getTaskById(task1.getId());
+        taskManager.deleteTaskById(task2.getId());
+        taskManager.getTaskById(task1.getId());
+
+        List<Task> history = taskManager.getHistory();
+        List<Task> testHistory = List.of(task4, epic1, subtask1, task3, task1);
+        Assertions.assertEquals(testHistory, history);
+    }
+
+    @Test
+    void historyOrderDeleteAll() {
+        TaskManager taskManager = Managers.getDefault();
+        Task task1 = new Task("task1", "task description1");
+        Task task2 = new Task("task2", "task description2");
+        Epic epic1 = new Epic("epic1", "epic description1");
+        taskManager.addEpic(epic1);
+        Subtask subtask1 = new Subtask("subtask1", "subtask description1", epic1.getId());
+        taskManager.addSubtask(subtask1);
+        taskManager.addTask(task1);
+        taskManager.addTask(task2);
+        taskManager.getTaskById(task1.getId());
+        taskManager.getTaskById(task2.getId());
+        taskManager.getSubtaskById(subtask1.getId());
+        taskManager.getEpicById(epic1.getId());
+        taskManager.deleteAllEpics();
+        taskManager.deleteAllTasks();
+
+        List<Task> history = taskManager.getHistory();
+        Assertions.assertEquals(0, history.size());
+    }
+}
