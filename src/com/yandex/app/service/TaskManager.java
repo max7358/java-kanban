22337e7530e0050524b17f237f6db984a3235e01
Max <maxusjob@ya.ru>
2024-04-19 19:45:14 +0200

    check style fix

--- src/com/yandex/app/service/TaskManager.java
+++ src/com/yandex/app/service/TaskManager.java
@@ -4,7 +4,6 @@ import com.yandex.app.model.Epic;
 import com.yandex.app.model.Subtask;
 import com.yandex.app.model.Task;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public interface TaskManager {
